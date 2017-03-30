ede1d48688c4a48da7bbf37ad1981f9bb4894460
aceslowman <aceslowman@gmail.com>
2017-03-30 16:04:53 -0600

    Updated my repository with all correct answers

--- basics.js
+++ basics.js
@@ -14,32 +14,94 @@ var arr = [12,42,36,51,96];
 // 2. Create a function called findTwo that takes in the arr array and returns
 // the value at index 2.
 
+function findTwo(arr){
+  return arr[2];
+}
+
 // 3. Create a function called pusher that takes in an array and a number and
 // positions the number at the end of the array. Return the new array.
+
+function pusher(arr, number){
+  arr.push(number);
+  return arr;
+}
 
 // 4. Create a function called upFront that takes in an array and a number and
 // positions the number in beginning of the array. Return the new array.
 
+function upFront(arr, number){
+  arr.unshift(number);
+  return arr;
+}
+
 // 5. Create a function called remover that takes in an array and removes the
 // value at the end of the array. Return the new array.
+
+function remover(arr){
+  arr.splice(-1,1);
+  return arr;
+}
 
 // 6. Create a function called copyCat that takes in an array, copies, and
 // returns the second and third index values.
 
+function copyCat(arr){
+  var copy = arr;
+  return [copy[2],copy[3]];
+}
+
 // 7. Create a function called chopIt that takes in an array and two numbers.
 // It deletes the original 3rd index value and inserts the two numbers in it's place.
 
+function chopIt(arr, num1, num2){
+  arr.splice(3, 1, num1, num2);
+  return arr;
+}
+
 // 8. Create a function called sumIt that takes in the array and returns the sum
 // of each of the values in the array.
+
+function sumIt(arr){
+  var sum = 0;
+
+  for(i = 0; i < arr.length; i++){
+    sum = sum + arr[i];
+  }
+
+  return sum;
+}
 
 // 9. Create a function called evenOdd that takes in the array and pushes into a
 // new array "even" or "odd" for every value in the array, depending upon
 // whether they are even or odd number values. Return the new array.
 // Hint: Use the modulus operator
 
+function evenOdd(arr){
+  var newArr = [];
+
+  for(i = 0; i < arr.length; i++){
+    var status;
+
+    if(arr[i] % 2){
+      status = "odd";
+    }else{
+      status = "even";
+    }
+
+    newArr.push(status);
+  }
+
+  return newArr;
+}
+
 // 10. Create a function called timesTen that takes in an array and multiplies
 // each value by 10 and returns the new array.
 // Hint: Use the map method
+
+function timesTen(arr){
+  var newArr = arr.map(function(x) { return x * 10; });
+  return newArr;
+}
 
 // Objects
 
@@ -47,32 +109,97 @@ var arr = [12,42,36,51,96];
 // keys. (species, name, age, gender, favoriteFood) Also, give the animal a
 // method of speech that returns a string that displays what the animal says.
 
+function Animal(){
+  this.species = "cat";
+  this.name = "bob";
+  this.age = 32;
+  this.gender = "Male";
+  this.favoriteFood = "Kibble";
+
+  this.speech = function() {
+    return "Hello!";
+  }
+}
+
+var animal = new Animal();
+
 // 2. Create a function called findAge that takes in an object and returns the
 // age key value.
+
+function findAge(obj){
+  return obj.age;
+}
 
 // 3. Create a function called getKeys that takes in an object and returns all
 // of the key names (not values!).
 
+function getKeys(obj){
+  return Object.keys(obj);
+}
+
 // 4. Create a function called changeName that takes in an object and a name
 // value and replaces the object name with the given name value. Return the new object.
+
+function changeName(obj, name){
+  obj.name = name;
+  return obj;
+}
 
 // 5. Create a function called speechMaker that takes in an object and returns
 // the response from calling the speech method.
 
+function speechMaker(obj){ return obj.speech(); }
+
 // 6. Create a function called addKey that takes in an object and adds a new key
 // name bedtime and assigns it a value. Return the object.
+
+function addKey(obj){
+  obj.bedtime = 10;
+  return obj;
+}
 
 // 7. Create a function called keyLoop that takes in an object and loops through
 // each of the key values. If any of the key values are equal to "monkey", then
 // return "There's a monkey!", otherwise return "There's no monkey here!".
+
+function keyLoop(obj){
+  for(var key in obj){
+    if(obj.hasOwnProperty(key)){
+      if(obj[key] == "monkey"){
+        return "There's a monkey!";
+      }else{
+        return "There's no monkey here!";
+      }
+    }
+  }
+}
 
 // Intermediate Arrays and Objects
 
 // 1. Create an array called cars of 3 objects of 3 different cars with the key
 // names of make, model, year.
 
+function Car(){
+  this.make = "Chevy";
+  this.model = "Silverado";
+  this.year = 1993;
+}
+
+var cars = [new Car(),new Car(),new Car()];
+
 // 2. Create a function called findModels that takes in an array, loops through
 // the array and returns an array of all of the model values of each object.
+
+function findModels(arr){
+  var newArray = [];
+
+  for(i = 0; i < arr.length; i++){
+    newArray.push(arr[i].model);
+  }
+
+  return newArray;
+}
+
 
 module.exports = {
   arr: arr,
